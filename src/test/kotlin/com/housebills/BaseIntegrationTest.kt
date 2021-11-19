1f31f7a67e6ddb66b914e0796e36c9f9f8111d59
Leandro Jesus <ajleandroaj@gmail.com>
2021-11-19 08:45:46 +0000

    test: Add DB cleanup after classTest runs and add support to H2 on tests

--- src/test/kotlin/com/housebills/BaseIntegrationTest.kt
+++ src/test/kotlin/com/housebills/BaseIntegrationTest.kt
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(OrderAnnotation::class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class BaseIntegrationTest
-// TODO: Add an after all to cleanup the DB
